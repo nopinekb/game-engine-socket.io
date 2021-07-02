b4cbd1d256d83919c69a5aa6885b08afbdc71672
nopinekb <33868882+nopinekb@users.noreply.github.com>
2021-07-02 15:06:08 +0500

    Add files via upload

---
+++ client.js
@@ -0,0 +1,96 @@
+//Establishing a connection with the server on port 5500y
+const socket = io('http://localhost:3000');
+
+
+var canvas = document.getElementById("canvas");
+
+var ctx = canvas.getContext("2d");
+var counter = document.getElementById('counter');
+var positionHTML = document.getElementById('position');
+var startBtn = document.getElementById('start');
+var x = 100, y = 100;
+var LoopSpeed = 10;
+var PlayerID = Math.floor(Math.random() * 1000000);
+var counterPlayers = 0;
+var sprite = new Image(20, 20);
+sprite.src = 'C:/Users/Никита/Documents/game/sprite.png';
+
+function PlayerControl() {
+	var LEFT=37, UP=38, RIGHT=39, DOWN=40;
+	var dirs = {[LEFT]:0, [UP]:0, [RIGHT]:0, [DOWN]:0};
+	var SPEED = 5;
+
+	$(document).keydown(function (e) {
+		dirs[e.keyCode] = 1;
+
+
+	})
+
+	$(document).keyup(function (e) {
+		dirs[e.keyCode] = 0;
+		
+	})
+	  
+	setInterval(function () {
+
+	x -= dirs[LEFT] * SPEED;
+	x += dirs[RIGHT] * SPEED;
+	y -= dirs[UP] * SPEED;
+	y += dirs[DOWN] * SPEED;
+
+	}, LoopSpeed)
+};
+
+
+	
+socket.on('draw', function(players){
+	
+	ctx.clearRect(0,0, 800,800);
+	for (var index = 0; index < players.length; ++index) {
+		ctx.drawImage(sprite, players[index].x, players[index].y);
+
+		ctx.fill();
+		positionHTML.innerHTML = JSON.stringify(players);
+	}
+
+});
+
+
+
+function positionUpdate() {
+	const position = {
+		PlayerID: PlayerID,
+		x: x,
+		y: y
+	};
+	socket.emit('position', position);
+	setTimeout(positionUpdate, LoopSpeed);
+}
+
+socket.on('count', function(count) {
+    counter.innerHTML = "online: " + count;
+    counterPlayers = count;
+});
+
+startBtn.addEventListener('click', () => {
+	const start = {
+		PlayerID: PlayerID,
+		x: x,
+		y: y
+	};
+    socket.emit('start', start);
+    startBtn.remove();
+})
+window.onbeforeunload = function() {
+	const end = {
+		PlayerID: PlayerID,
+		x: x,
+		y: y
+	};
+    socket.emit('end', end);
+};
+
+
+positionUpdate();
+PlayerControl();
+
---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const server = app.listen(port)
+const io = require('socket.io')(server)
+
+//Hello World line taken from the express website
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+let players = [];
+//The 'connection' is a reserved event name in socket.io
+//For whenever a connection is established between the server and a client
+io.on('connection', (socket) => {
+
+	//Displaying a message on the terminal
+    console.log('A user connected. Now ' + io.engine.clientsCount + ' players');
+
+
+    socket.on('disconnect', function () {
+        console.log('A user disconnected. Now ' + io.engine.clientsCount + ' players');
+    });
+
+
+    var count = io.engine.clientsCount;
+    socket.emit('count', count);
+
+    
+
+    function UpdatePlayer(id,x,y){
+         for (var index = 0; index < players.length; ++index) {
+            if(players[index].id === id){
+                players[index].x = x, players[index].y = y;
+            }
+        }
+        socket.emit('draw', players);
+        console.log(players); 
+    }
+    function MakePlayer(id,x,y) {      
+        if (players.filter(item=> item.id == id).length == 0){
+            players.push({id, x, y});
+        }
+    }
+
+    function DeletePlayer(id){
+        for (var index = 0; index < players.length; ++index) {
+             if(players[index].id === id){
+                players.splice(index,1);
+            }
+        }
+    }
+
+    socket.on('position', function(position) {
+        UpdatePlayer(position.PlayerID,position.x,position.y);
+    });
+
+    socket.on('start', function(start) {
+        MakePlayer(start.PlayerID,start.x,start.y)
+    });
+
+    socket.on('end', function(end) {
+        DeletePlayer(end.PlayerID,end.x,end.y)
+    });
+
+
+                
+            
+});
+
